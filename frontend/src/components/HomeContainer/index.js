63a17185b0e5deb0a84f57b9ab945d687fe12687
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-04-01 19:37:11 -0400

    bottom nav

--- frontend/src/components/HomeContainer/index.js
+++ frontend/src/components/HomeContainer/index.js
@@ -1,17 +1,26 @@
+import { BottomNavigation } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import {Redirect} from 'react-router-dom'
-import MyWeeklySummary from '../MyWeeklySummary';
+import BottomNav from '../BottomNav';
+import CoachesCorner from './CoachesCorner';
+import MyWeeklySummary from './MyWeeklySummary';
 
 
 
 function HomeContainer() {
   const sessionUser = useSelector(state => state.session.user);
+  
 
   if(!sessionUser) return <Redirect to="/"/>
   
   return(
-    <MyWeeklySummary />
+    <>
+      <MyWeeklySummary />
+      <CoachesCorner />
+      <BottomNav />
+      
+    </>
   )
 }
 
