b255497eb06c4d327ea1b2b3f11d058f2c2222a2
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-04-01 15:39:35 -0400

    added some alignment

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import LoginFormModal from '../Login_Signup/LoginFormModal';
 import QuestionModal from '../Login_Signup/QuestionModal'
+import './SplashPage.css'
 
 function SplashPage(){
   const sessionUser = useSelector(state => state.session.user);
@@ -10,9 +11,13 @@ function SplashPage(){
 
 return (
   <>
-    <Typography variant="h2" color="primary">Welcome to Teamsight</Typography>
-    <LoginFormModal> Login! </LoginFormModal>
-    <QuestionModal> Sign Up!</QuestionModal>
+    <div className="container">
+      <Typography variant="h2" color="primary">Welcome to Teamsight</Typography>
+      <div className="buttons"> 
+        <LoginFormModal> Login! </LoginFormModal>
+        <QuestionModal> Sign Up!</QuestionModal>
+      </div>
+    </div>
   </>
 )
 
