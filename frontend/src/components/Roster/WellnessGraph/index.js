3eb46b7568267bacf655d66baa8c840b9001e0bc
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-04-12 09:35:22 -0500

    added the dummy chart on coach data

--- frontend/src/components/Roster/WellnessGraph/index.js
+++ frontend/src/components/Roster/WellnessGraph/index.js
@@ -1,42 +1,56 @@
 import { Typography } from "@material-ui/core";
+import {LineChart,CartesianGrid,XAxis,YAxis,Tooltip,Legend,Line,} from "recharts";
 import "./WellnessGraph.css"
+
+const data = [
+  {
+    name: "October",
+    average: 2.6,
+  },
+  {
+    name: "November",
+    average: 3.7,
+  },
+  {
+    name: "December",
+    average: 4.9,
+  },
+  {
+    name: "January",
+    average: 1.8,
+  },
+  {
+    name: "February ",
+    average: 3.5,
+  },
+  {
+    name: "March",
+    average: 4.8,
+  }
+];
+
 
 
 function WellnessGraph() {
 
-  // const data = {
-  //   labels: ['September', 'October', 'November', 'December', 'January', 'February', 'March'],
-  //   datasets: [
-  //     {
-  //     label: 'Dataset of Months',
-  //     fill: false,
-  //     lineTension: 0.1,
-  //     backgroundColor: 'rgba(75,192,192,0.4)',
-  //     borderColor: 'rgba(75,192,192,1)',
-  //     borderCapStyle: 'butt',
-  //     borderDash: [],
-  //     borderDashOffset: 0.0,
-  //     borderJoinStyle: 'miter',
-  //     pointBorderColor: 'rgba(75,192,192,1)',
-  //     pointBackgroundColor: '#fff',
-  //     pointBorderWidth: 1,
-  //     pointHoverRadius: 5,
-  //     pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-  //     pointHoverBorderColor: 'rgba(220,220,220,1)',
-  //     pointHoverBorderWidth: 2,
-  //     pointRadius: 1,
-  //     pointHitRadius: 10,
-  //     data: [65, 59, 80, 81, 56, 55, 40]
-  //     }
-  //   ]
-  // }
   return(
     <>
       <div className="wellness-title">
         <Typography variant="h4">Your Team's Wellness For The past 6 months</Typography>
       </div>
       <div className="wellness-graph">
-        {/* <Line data={data}/> */}
+        <LineChart width={800}
+        height={400}
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="average" stroke="#8884d8" />
+        </LineChart>
       </div>
     </>
   )
