a18f0e29ee62b54e9f523c63aa1cf834e8a72815
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-04-14 17:02:15 -0500

    styled the edit and new psot forms well

--- frontend/src/components/CoachPosts/EditPostModal/EditPostForm.js
+++ frontend/src/components/CoachPosts/EditPostModal/EditPostForm.js
@@ -3,10 +3,18 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TextField from '@material-ui/core/TextField';
 import { deletePost, editPost } from "../../../store/posts";
+import { makeStyles } from '@material-ui/core/styles';
+import './EditPostForm.css'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: `rgba(${127}, ${125}, ${227}, ${.2})`
+  }
+}))
 
 export default function EditPostForm({setShowModal, id}) {
   const dispatch = useDispatch()
+  const classes = useStyles()
   const coachId = useSelector(state => state.session.user.id)
   const teamId = useSelector(state => state.session.user.teamId)
   const existingPost = useSelector(state => state.posts[id])
@@ -36,25 +44,41 @@ export default function EditPostForm({setShowModal, id}) {
   }
   return(
     <div className="edit-post-form">
-      <Typography variant="h4" color="primary">inspire your athletes!</Typography>
+      <div className='edit-post-title'>
+        <Typography variant="h4" color="primary">inspire your athletes!</Typography>
+      </div>
       <form>
-        <TextField id="standard-basic" placeholder="answer an athlete's question or type one here" 
-          label="title" value={title} multiline
-          onChange={(e) => setTitle(e.target.value)} required/>
-        <TextField id="standard-basic" label="body" placeholder="the body of your post" value={post}
-          multiline onChange={(e) => setPost(e.target.value)} required/>
-        <div className="post-media-upload">
-            <Typography variant="body1">Choose an image for your post!</Typography>
-            <input type="file" onChange={updateFile}/>
+        <div className='all-edit-post-fields'>
+          <div className="edit-post-inputs">
+            <TextField id="standard-basic" placeholder="answer an athlete's question or type one here" 
+              label="title" value={title} multiline className={classes.root} fullWidth
+              onChange={(e) => setTitle(e.target.value)} required/>
+          </div>
+          <div className="edit-post-inputs">
+            <TextField id="standard-basic" label="body" placeholder="the body of your post" value={post}
+              multiline onChange={(e) => setPost(e.target.value)} required className={classes.root}
+              fullWidth/>
+          </div>
+          <div className="post-media-upload">
+              <Typography variant="body1">Choose an image for your post!</Typography>
+              <input type="file" onChange={updateFile}/>
+          </div>
+          <div className="edit-post-inputs">
+            <TextField id="standard-basic" label="link" placeholder="copy a link to a video or article here!" value={externalLink}
+              multiline onChange={(e) => setExternalLink(e.target.value)} className={classes.root}
+              fullWidth/>
+          </div>
+          <div className="edit-post-button">
+            <Button variant="outlined" color="primary" onClick={handleEditPost} fullWidth>
+              Save Changes
+            </Button>
+          </div>
+          <div className="delete-post-button">
+            <Button variant="outlined" color="secondary" onClick={handleDeletePost} fullWidth>
+              Delete Post
+            </Button>
+          </div>
         </div>
-        <TextField id="standard-basic" label="link" placeholder="copy a link to a video or article here!" value={externalLink}
-          multiline onChange={(e) => setExternalLink(e.target.value)}/>
-        <Button variant="outlined" color="primary" onClick={handleEditPost}>
-          Save Changes
-        </Button>
-        <Button variant="outlined" color="secondary" onClick={handleDeletePost}>
-          Delete Post
-        </Button>
       </form>
     </div>
   )
--- frontend/src/components/CoachPosts/NewPostModal/NewPostForm.js
+++ frontend/src/components/CoachPosts/NewPostModal/NewPostForm.js
@@ -3,11 +3,19 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TextField from '@material-ui/core/TextField';
 import { createPost } from "../../../store/posts";
+import { makeStyles } from '@material-ui/core/styles';
 import './NewPostForm.css'
 
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: `rgba(${127}, ${125}, ${227}, ${.2})`
+  }
+}))
+
 export default function NewPostForm({setShowModal}) {
   const dispatch = useDispatch()
+  const classes = useStyles()
   const coachId = useSelector(state => state.session.user.id)
   const teamId = useSelector(state => state.session.user.teamId)
 
@@ -36,17 +44,26 @@ export default function NewPostForm({setShowModal}) {
       </div>
       <form>
         <div className="all-new-post-fields">
-            <TextField variant="standard" placeholder="answer an athlete's question or type one here" 
+          <div className="new-post-input">
+            <TextField variant="outlined" placeholder="answer an athlete's question or type one here" 
               label="title" value={title} multiline
-              onChange={(e) => setTitle(e.target.value)} required/>
-          <TextField variant="standard" label="body" placeholder="the body of your post" value={post}
-            multiline className="new-post-input" onChange={(e) => setPost(e.target.value)} required/>
+              onChange={(e) => setTitle(e.target.value)} required fullWidth className={classes.root}/>
+          </div>
+          <div className="new-post-input">
+          <TextField variant="outlined" label="body" placeholder="the body of your post" value={post}
+            multiline className="new-post-input" onChange={(e) => setPost(e.target.value)} 
+            className={classes.root} required fullWidth/>
+          </div>
           <div className="post-media-upload">
               <Typography variant="body1">Choose an image for your post!</Typography>
               <input className="choose-file-post" type="file" onChange={updateFile}/>
           </div>
-          <TextField variant="standard" label="link" placeholder="copy a link to a video or article here!" value={externalLink}
-            multiline className="new-post-input" onChange={(e) => setExternalLink(e.target.value)}/>
+          <div className="new-post-input">
+          <TextField variant="outlined" label="link" placeholder="copy a link to a video or article here!" value={externalLink}
+            multiline className="new-post-input" 
+            className={classes.root} onChange={(e) => setExternalLink(e.target.value)}
+            fullWidth/>
+          </div>
           <div className="new-post-button">
             <Button variant="outlined" color="primary" onClick={handleNewPost}>
               Create Post!
