3bd3adcab4e672e9ad16c16e9c971f605e0ebfe0
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-04-17 10:58:23 -0500

    changed the profil styling

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -1,8 +1,9 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { Typography } from "@material-ui/core";
+import { TextField, Typography } from "@material-ui/core";
 import Navigation from '../Navigation'
 import { useEffect } from 'react';
 import { getOneTeam } from '../../store/team';
+import './Profile.css'
 
 
 function Profile(){
@@ -16,21 +17,35 @@ function Profile(){
     await dispatch(getOneTeam(sessionUser.teamId))
   },[dispatch])
 
+  
   return (
     team && 
     <>
       <Navigation/>
-      <div>
-        <Typography variant="h3" color="primary">My Profile</Typography>
-        <Typography variant="body1" color="primary">
-          name: {sessionUser.firstName} {sessionUser.lastName}
-        </Typography>
-        <Typography variant="body1" color="primary">
-          email: {sessionUser.email}
-        </Typography>
-        <Typography variant="body1" color="primary">
-          team: {team[0].teamName}
-        </Typography>
+
+      <div className="my-profile-container">
+        <div className="my-profile-title">
+          <Typography variant="h3" color="primary">My Profile</Typography>
+        </div>
+        <div className="my-profile-info">
+          <TextField variant="outlined"  label="First Name" color="primary"
+          value={sessionUser.firstName} disabled
+          style={{backgroundColor: `rgba(${127}, ${125}, ${227}, ${.2})`}}>
+          </TextField>
+          <TextField variant="outlined"  label="Last Name" color="primary"
+          value={sessionUser.lastName} disabled
+          style={{backgroundColor: `rgba(${127}, ${125}, ${227}, ${.2})`}}>
+          </TextField>
+          <TextField variant="outlined"  label="Email" color="primary"
+          value={sessionUser.email} disabled
+          style={{backgroundColor: `rgba(${127}, ${125}, ${227}, ${.2})`}}>
+          </TextField>
+          <div className="team-logo-profile">
+            <Typography variant="body1" color="primary">
+              {/* team: {team[0].teamName} */}
+            </Typography>
+          </div>
+        </div>
         
       </div>
     </>
