c3b60c33a756d83bc64ad47e47a02b03a9b31aa1
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-04-18 21:47:09 -0500

    doing good work on the landing page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -6,34 +6,26 @@ import Carousel from 'react-material-ui-carousel'
 import './SplashPage.css'
 import SplashNav from './SplashNav.js';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minHeight: '100vh',
-    backgroundImage: './phelps-fly.jpeg'
-  }
-}))
 
 function SplashPage(){
-  const classes = useStyles()
   const dispatch = useDispatch()
   const history = useHistory()
 
 
 
 return (
-  <div className={classes.root}>
+  <div className="splash-container">
     <div className= 'splash-header'>
       <SplashNav/>
       <div>
         
       </div>
       <div className="athlete-title-splash">
-        <p>I am the best ever</p>
         <Typography variant="h4" color="primary">Every great athlete...</Typography>
       </div>
-      {/* <div className="coach-title-splash">
+      <div className="coach-title-splash">
         <Typography variant="h4" color="primary">Starts with a great coach</Typography>
-      </div>  */}
+      </div> 
     </div>
     {/* <div className="carousel-splash">
       {/* <Carousel
